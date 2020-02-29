13a70d6b555d142961e017fda1d5ceef8986a9f0
Ivan Reshetnyak <resh0607@gmail.com>
2020-02-29 23:36:14 +0300

    Add null constraint to answers body attr.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_202723) do
+ActiveRecord::Schema.define(version: 2020_02_29_203507) do
 
   create_table "answers", force: :cascade do |t|
-    t.text "body"
+    t.text "body", null: false
     t.boolean "correct", default: false
     t.integer "question_id"
     t.datetime "created_at", null: false
