e29e6b2f2cf6efc44083ba1d81e3822c78eafa06
Ivan Reshetnyak <resh0607@gmail.com>
2020-02-29 23:00:19 +0300

    Add default value to answers correct attr

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_195507) do
+ActiveRecord::Schema.define(version: 2020_02_29_195729) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
-    t.boolean "correct"
+    t.boolean "correct", default: false
     t.integer "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
