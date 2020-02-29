5f121f6e4ef65d600243a39ff8218d9c583f7c8a
Ivan Reshetnyak <resh0607@gmail.com>
2020-02-29 19:43:24 +0300

    Add model User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_160826) do
+ActiveRecord::Schema.define(version: 2020_02_29_162107) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 2020_02_29_160826) do
     t.text "title"
     t.integer "level"
     t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.text "name"
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
