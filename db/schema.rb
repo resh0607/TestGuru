79880144d00d574116eabfbf44f83a10b9972950
Ivan Reshetnyak <resh0607@gmail.com>
2020-02-29 19:10:18 +0300

    Add model Answer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_074900) do
+ActiveRecord::Schema.define(version: 2020_02_29_160826) do
+
+  create_table "answers", force: :cascade do |t|
+    t.text "body"
+    t.boolean "correct"
+    t.integer "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.text "body"
