a3127cb0a8154b0bd46b94fccbb5ba548444b3e3
Ivan Reshetnyak <resh0607@gmail.com>
2020-02-29 23:20:23 +0300

    Add null constraint to tests title attr.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_195729) do
+ActiveRecord::Schema.define(version: 2020_02_29_200338) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2020_02_29_195729) do
   end
 
   create_table "tests", force: :cascade do |t|
-    t.text "title"
+    t.text "title", null: false
     t.integer "level", default: 0
     t.integer "category_id"
     t.datetime "created_at", null: false
