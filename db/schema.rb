8b84810810d958bcee62a143f95a8d610534629f
Ivan Reshetnyak <resh0607@gmail.com>
2020-02-29 22:56:53 +0300

    Add default value to tests level attr

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_164502) do
+ActiveRecord::Schema.define(version: 2020_02_29_195507) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2020_02_29_164502) do
 
   create_table "tests", force: :cascade do |t|
     t.text "title"
-    t.integer "level"
+    t.integer "level", default: 0
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
