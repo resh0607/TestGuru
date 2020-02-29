931971665ea853aab3f1268a601f5c79ab02e770
Ivan Reshetnyak <resh0607@gmail.com>
2020-02-29 23:38:17 +0300

    Add null constraint to users name attr.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_203507) do
+ActiveRecord::Schema.define(version: 2020_02_29_203659) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2020_02_29_203507) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.text "name"
+    t.text "name", null: false
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
