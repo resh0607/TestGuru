189aab3a69d112352529e607b0b90bb3a58c7506
Ivan Reshetnyak <resh0607@gmail.com>
2020-03-19 20:13:14 +0300

    Add tests_by_level method to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  
+  def tests_by_level(level)
+    Test.joins("JOIN tests_users ON tests.id = tests_users.user_id").where("tests_users.user_id = :user_id AND level = :level", level: level, user_id: id)
+  end
 end
