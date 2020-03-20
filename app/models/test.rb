fe1a2a456502d5d6c3c1332dd611655416f5f297
Ivan Reshetnyak <resh0607@gmail.com>
2020-03-20 22:59:20 +0300

    Add by_category method to Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+
+  def self.by_category(category)
+    Test.joins("JOIN categories ON tests.category_id = categories.id").where(categories: {title: category}).order(:title).reverse_order.pluck(:title)
+  end
 end
