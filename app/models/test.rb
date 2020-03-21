1096657c0b20ffbd2cc243392b84649b51779ba8
Ivan Reshetnyak <resh0607@gmail.com>
2020-03-21 21:54:49 +0300

    Fix by_category method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
 
   def self.by_category(category)
-    Test.joins("JOIN categories ON tests.category_id = categories.id").where(categories: {title: category}).order(:title).reverse_order.pluck(:title)
+    Test.joins("JOIN categories ON tests.category_id = categories.id").where(categories: {title: category}).order(title: :desc).pluck(:title)
   end
 end
