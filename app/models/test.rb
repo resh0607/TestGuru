26a2c9b9724c46097f4378a7321170d5be62a843
Ivan Reshetnyak <resh0607@gmail.com>
2020-03-24 22:36:40 +0300

    Update associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,7 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
   has_and_belongs_to_many :users
+  belongs_to :author, class_name: "User", foreign_key: "user_id"
 
   # def self.by_category(category)
   #   Test.joins("JOIN categories ON tests.category_id = categories.id").where(categories: {title: category}).order(title: :desc).pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :tests
- 
+  has_many :authored_test, class_name: 'Test'
   # def tests_by_level(level)
   #   Test.joins("JOIN tests_users ON tests.id = tests_users.test_id").where(tests_users:{user_id: id}, level: level)
   # end
