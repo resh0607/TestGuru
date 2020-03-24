0fcfc194a13250536b46360290b675c61e586a58
Ivan Reshetnyak <resh0607@gmail.com>
2020-03-24 22:10:36 +0300

    Add associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,9 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+  has_and_belongs_to_many :users
 
-  def self.by_category(category)
-    Test.joins("JOIN categories ON tests.category_id = categories.id").where(categories: {title: category}).order(title: :desc).pluck(:title)
-  end
+  # def self.by_category(category)
+  #   Test.joins("JOIN categories ON tests.category_id = categories.id").where(categories: {title: category}).order(title: :desc).pluck(:title)
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  has_and_belongs_to_many :tests
  
-  def tests_by_level(level)
-    Test.joins("JOIN tests_users ON tests.id = tests_users.test_id").where(tests_users:{user_id: id}, level: level)
-  end
+  # def tests_by_level(level)
+  #   Test.joins("JOIN tests_users ON tests.id = tests_users.test_id").where(tests_users:{user_id: id}, level: level)
+  # end
 end
