2c55600d506d23a2608190490e2ce9384416fc7c
Ivan Reshetnyak <resh0607@gmail.com>
2020-03-24 22:59:07 +0300

    Add user_id to Test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_142812) do
+ActiveRecord::Schema.define(version: 2020_03_24_193755) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_03_06_142812) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
