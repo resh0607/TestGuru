24e0f4368a72db578986f0408ef501021bdabaca
Ivan Reshetnyak <resh0607@gmail.com>
2020-03-25 23:03:29 +0300

    Add user - tests reference

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_142812) do
+ActiveRecord::Schema.define(version: 2020_03_25_195135) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -41,7 +41,9 @@ ActiveRecord::Schema.define(version: 2020_03_06_142812) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
   create_table "tests_users", id: false, force: :cascade do |t|
