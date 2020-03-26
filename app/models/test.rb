a5850e757207cbaf8e43a279ecfc36b70a6fcee5
Ivan Reshetnyak <resh0607@gmail.com>
2020-03-26 11:01:54 +0300

    Add destroy dependence to models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,10 +1,10 @@
 class Test < ApplicationRecord
   belongs_to :category
-  has_many :questions
-  has_and_belongs_to_many :users
   belongs_to :author, class_name: "User", foreign_key: "user_id"
-
-  # def self.by_category(category)
-  #   Test.joins("JOIN categories ON tests.category_id = categories.id").where(categories: {title: category}).order(title: :desc).pluck(:title)
-  # end
+  has_many :questions, dependent: :destroy
+  has_and_belongs_to_many :users
+  
+  def self.by_category(category)
+    Test.joins("JOIN categories ON tests.category_id = categories.id").where(categories: {title: category}).order(title: :desc).pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :tests
-  has_many :authored_tests, class_name: 'Test'
-  # def tests_by_level(level)
-  #   Test.joins("JOIN tests_users ON tests.id = tests_users.test_id").where(tests_users:{user_id: id}, level: level)
-  # end
+  has_many :authored_tests, class_name: 'Test', dependent: :destroy
+
+  def tests_by_level(level)
+    Test.joins("JOIN tests_users ON tests.id = tests_users.test_id").where(tests_users:{user_id: id}, level: level)
+  end
 end
