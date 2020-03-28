ad6c1cde6abebd4d1267fa57658b659e905351c6
Ivan Reshetnyak <resh0607@gmail.com>
2020-03-28 13:58:56 +0300

    Refactoring

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,8 +4,8 @@ class Test < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :tests_users
   has_many :users, through: :tests_users
-  
+
   def self.by_category(category)
-    Test.joins("JOIN categories ON tests.category_id = categories.id").where(categories: {title: category}).order(title: :desc).pluck(:title)
+    Test.joins(:category).where(categories: {title: category}).order(title: :desc).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :authored_tests, class_name: 'Test', dependent: :destroy
 
   def tests_by_level(level)
-    Test.joins("JOIN tests_users ON tests.id = tests_users.test_id").where(tests_users:{user_id: id}, level: level)
+    Test.joins(:tests_users).where(tests_users:{user_id: id}, level: level)
   end
 end
