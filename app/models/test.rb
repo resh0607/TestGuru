8805ecbdd268af7a21a157eb584b727c3909e54a
Ivan Reshetnyak <resh0607@gmail.com>
2020-03-29 12:48:25 +0300

    Update dependencies

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,7 +3,7 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "user_id"
   has_many :questions, dependent: :destroy
   has_many :tests_users
-  has_many :users, through: :tests_users
+  has_many :users, through: :tests_users, dependent: :destroy
 
   def self.by_category(category)
     Test.joins(:category).where(categories: {title: category}).order(title: :desc).pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :tests_users
-  has_many :tests, through: :tests_users
-  has_many :authored_tests, class_name: 'Test', dependent: :destroy
+  has_many :tests, through: :tests_users, dependent: :destroy
+  has_many :authored_tests, class_name: 'Test', dependent: :nullify
 
   def tests_by_level(level)
     Test.joins(:tests_users).where(tests_users:{user_id: id}, level: level)
