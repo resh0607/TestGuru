f08c8a32564872e9e4abf13fbec02bc954a68f88
Ivan Reshetnyak <resh0607@gmail.com>
2020-03-30 12:27:29 +0300

    Add scopes

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,6 +5,12 @@ class Test < ApplicationRecord
   has_many :tests_users
   has_many :users, through: :tests_users, dependent: :destroy
 
+  scope :by_level, -> (level) { where(level: level) }
+  scope :easy, -> { by_level(0..1) }
+  scope :medium, -> { by_level(2..4) }
+  scope :difficult, -> { by_level(5..Float::INFINITY) }
+
+
   def self.by_category(category)
     Test.joins(:category).where(categories: {title: category}).order(title: :desc).pluck(:title)
   end
