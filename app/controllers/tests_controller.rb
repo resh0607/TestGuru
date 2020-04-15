e5c1e1a0ce605ccc06cc9aba8f18f67d9b3f9c9f
Ivan Reshetnyak <resh0607@gmail.com>
2020-04-15 12:36:22 +0300

    Add new/create functional for 'test'

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -9,9 +9,26 @@ class TestsController < ApplicationController
     @questions = @test.questions
   end
 
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
   private
 
   def find_test
     @test = Test.find(params[:id])
+  end
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :user_id)
   end
 end
