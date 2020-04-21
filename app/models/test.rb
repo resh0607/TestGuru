472a1e4c90f3a35bf2ca4a17c3069176bfb9c2db
Ivan Reshetnyak <resh0607@gmail.com>
2020-04-21 13:22:21 +0300

    Update relations depends TestPassage model

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,8 +2,8 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: "User", foreign_key: "user_id"
   has_many :questions, dependent: :destroy
-  has_many :tests_users
-  has_many :users, through: :tests_users, dependent: :destroy
+  has_many :test_passages
+  has_many :users, through: :test_passages, dependent: :destroy
 
   validates :title, presence: true,
                     uniqueness: { scope: :level,
