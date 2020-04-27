63aefd84c82258e4f8f192e74d4ad7cbe9ebee74
Ivan Reshetnyak <resh0607@gmail.com>
2020-04-27 17:41:06 +0300

    Add alert messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to tests_path
     else
+      flash.now[:alert] = 'Check your credentials'
       render :new
     end
   end
