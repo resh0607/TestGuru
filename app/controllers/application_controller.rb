7f6ee5b95b94263493897df83672829953cb7ee1
Ivan Reshetnyak <resh0607@gmail.com>
2020-05-08 11:00:41 +0300

    Use cookies instead of session in redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,10 +22,11 @@ class ApplicationController < ActionController::Base
   end
 
   def store_location
-    session[:forwarding_url] = request.original_url if request.get?
+    cookies[:forwarding_url] = request.original_url if request.get?
   end
 
   def redirect_back_or(default)
-    redirect_to(session[:forwarding_url] || default)
+    redirect_to(cookies[:forwarding_url] || default)
+    cookies.delete(:forwarding_url)
   end
 end
