f87d7026e604d942447817bbb5137bc50aef5d15
Ivan Reshetnyak <resh0607@gmail.com>
2020-05-09 10:30:01 +0300

    Minor fixes(depends devise)

--- db/migrate/20200508143748_add_devise_to_users.rb
+++ db/migrate/20200508143748_add_devise_to_users.rb
@@ -39,7 +39,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.2]
     remove_column(:users, :password_digest)
     change_column_default(:users, :email, '')
 
-    add_index :users, :email,                unique: true
+    # add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
