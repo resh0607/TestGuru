f743f678bf8bb10f3666fb9911ff69c49dba400f
Ivan Reshetnyak <resh0607@gmail.com>
2020-05-12 21:09:21 +0300

    Add access rights

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,11 @@
+class Admin::BaseController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :admin_required!
+
+  private
+
+  def admin_required!
+    redirect_to root_path, alert: 'You have no access to this page' unless current_user.is_a?(Admin)
+  end
+end
