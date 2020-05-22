3bf22612f1b63a3564030f0da9cae21d4739e58b
Ivan Reshetnyak <resh0607@gmail.com>
2020-05-22 20:16:43 +0300

    Add 'Test' resource locale

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,9 +1,9 @@
 module TestsHelper
-  def test_header(test)
-    if test.new_record?
-      'Create new test'
-    else
-      'Edit test'
-    end
-  end
+  # def test_header(test)
+  #   if test.new_record?
+  #     'Create new test'
+  #   else
+  #     'Edit test'
+  #   end
+  # end
 end
