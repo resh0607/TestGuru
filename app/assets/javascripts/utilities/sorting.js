bc837d74d2f25e3787e704b524151e4758ce04a6
Ivan Reshetnyak <resh0607@gmail.com>
2020-06-15 12:48:07 +0300

    Add Asc/Desc arrows to sorting

--- app/assets/javascripts/utilities/sorting.js
+++ app/assets/javascripts/utilities/sorting.js
@@ -16,12 +16,24 @@ function sortRowsByTitle() {
     sortedRows.push(rows[i])
   }
   
-  sortedRows.sort(compareRows)
+
+  // var arrowUp = this.querySelector('.fas fa-arrow-up')
+
+  if (this.querySelector('.fa-arrow-up').classList.contains('hide')) {
+    sortedRows.sort(compareRowsAsc)
+    this.querySelector('.fa-arrow-up').classList.remove('hide')
+    this.querySelector('.fa-arrow-down').classList.add('hide')
+  } else {
+    sortedRows.sort(compareRowsDesc)
+    this.querySelector('.fa-arrow-up').classList.add('hide')
+    this.querySelector('.fa-arrow-down').classList.remove('hide')
+  }
+  
 
   var sortedTable = document.createElement('table')
 
   sortedTable.classList.add('table')
-  sortedTable.appendChild(rows[0])
+  sortedTable.appendChild(rows[0]).classList.add('thead-dark')
 
   for (var i = 0; i < sortedRows.length; i++) {
     sortedTable.appendChild(sortedRows[i])
@@ -31,11 +43,20 @@ function sortRowsByTitle() {
   table.parentNode.replaceChild(sortedTable, table)
 }
 
-function compareRows(row1, row2) {
+function compareRowsAsc(row1, row2) {
   var testTitle1 = row1.querySelector('td').textContent
   var testTitle2 = row2.querySelector('td').textContent
 
   if (testTitle1 < testTitle2) { return -1 }
   if (testTitle1 > testTitle2) { return  1 }
+  return 0
+}
+
+function compareRowsDesc(row1, row2) {
+  var testTitle1 = row1.querySelector('td').textContent
+  var testTitle2 = row2.querySelector('td').textContent
+
+  if (testTitle1 < testTitle2) { return 1 }
+  if (testTitle1 > testTitle2) { return -1 }
   return 0
 }
