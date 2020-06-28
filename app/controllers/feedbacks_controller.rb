df8acbbcce8c82079763d1a26f74a13327cc1070
Ivan Reshetnyak <resh0607@gmail.com>
2020-06-28 14:36:19 +0300

    Add new/create methods to feedback controller

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,8 +1,22 @@
 class FeedbacksController < ApplicationController
 
   def new
+    @feedback = Feedback.new
   end
 
   def create
+    @feedback = Feedback.new(feedback_params)
+    if @feedback.save
+      flash[:notice] = 'We wil try to answer you as soon as possible'
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def feedback_params
+    params.require(:feedback).permit(:email, :text_body)
   end
 end
