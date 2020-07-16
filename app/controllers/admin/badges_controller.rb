e0b59f610f2024255b6c5af1a37b8bb8258f38a3
Ivan Reshetnyak <resh0607@gmail.com>
2020-07-16 19:33:05 +0300

    Implement Admin::BadgesController

---
+++ app/controllers/admin/badges_controller.rb
@@ -0,0 +1,45 @@
+class Admin::BadgesController < Admin::BaseController
+  before_action :set_badge, only: [:edit, :update, :destroy]
+
+  def index
+    @badges = Badge.all
+  end
+
+  def new
+    @badge = Badge.new
+  end
+
+  def edit
+  end
+
+  def create
+    @badge = Badge.new(badge_params)
+    if @badge.save
+      redirect_to admin_badges_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @badge.update(badge_params)
+      redirect_to admin_badges_path
+    else
+      redirect_to :edit
+    end
+  end
+
+  def destroy
+    @badge.destroy
+    redirect_to admin_badges_path
+  end
+
+  private
+    def set_badge
+      @badge = Badge.find(params[:id])
+    end
+
+    def badge_params
+      params.require(:badge).permit(:name, :rule_name, :image_url)
+    end
+end
