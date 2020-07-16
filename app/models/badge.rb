6aed2dd6222fd2da0dae2be9b9bd566bf9251825
Ivan Reshetnyak <resh0607@gmail.com>
2020-07-16 19:32:17 +0300

    Add Badge validations

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,13 +1,11 @@
 class Badge < ApplicationRecord
 
-  GIVING_RULES = %w[
-                    test_in_one_attempt
-                    category_guru
-                    level_guru
-                    ] 
-
   has_many :achievements, dependent: :destroy
   has_many :users, through: :achievements
 
-  
+  validates :name, :rule_name, presence: true
+  validates_uniqueness_of :name, case_sensitive: false
+  # validates_uniqueness_of :rule_name, case_sensitive: false
+  validates :rule_name, inclusion: { in: BadgeService::BADGE_GIVING_RULES, 
+                                     message: "There is no such rule" }
 end
