33de35a8ebf6528f2f0527676cc413c6af17ea85
Ivan Reshetnyak <resh0607@gmail.com>
2020-07-22 14:33:46 +0300

    update test`s index view

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -6,4 +6,22 @@ module TestsHelper
   #     'Edit test'
   #   end
   # end
+
+  def active(test)
+    unless test_passage_active?(test)
+      '<i class="fas fa-play-circle"></i>'
+    end
+  end
+
+  def start_button(test)
+    if test_passage_active?(test)
+      t('.start')
+    else
+      t('.continue')
+    end
+  end
+
+  def test_passage_active?(test)
+    current_user.test_passage(test).nil? || current_user.test_passage(test).completed?
+  end
 end
